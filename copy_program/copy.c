792be6563a45804f3054655a2fb1537415b6b2b4
electroons <devesh@electroons.com>
2017-02-19 11:48:43 +0530

    file copy example added

---
+++ copy_program/copy.c
@@ -0,0 +1,41 @@
+#include<fcntl.h>
+
+char buffer[2048];
+int version =1 ;
+
+int main(int argc,char *argv[])
+{
+	int fdold, fdnew;
+	
+	if(argc != 3)
+	{
+		printf("Usage : copy <oldfile> <newfile>\n");
+		exit(1);
+	}
+
+	fdold = open(argv[1], O_RDONLY);
+
+	if(fdold == -1)
+	{
+		printf("cannot open source file\n");
+		exit(1);
+	}
+
+	fdnew = creat(argv[2], 0666);
+
+	if(fdnew == -1)
+	{
+		printf("cannot create file %s \n", argv[2]);
+		exit(1);
+	}
+
+	copy(fdold, fdnew);
+	exit(0);
+}
+
+copy(int old, int new)
+{
+	int count;
+	while((count=read(old, buffer, sizeof(buffer)))>0)
+		write(new, buffer, count);
+}
